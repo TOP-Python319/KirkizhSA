c3c17506ae378d3accdd1065d984f7813ebe329f
StanislavAK <kirkizhsa@mail.ru>
2024-06-05 21:14:23 +0500

    выполненное домашнее задание

---
+++ HW_2024_05_24/1-1.py
@@ -0,0 +1,37 @@
+class Money:
+    def __init__(self, dollars, cents):
+        self.total_cents = dollars * 100 + cents
+
+    @property
+    def dollars(self):
+        return self.total_cents // 100
+
+    @dollars.setter
+    def dollars(self, value):
+        if value >= 0:
+            self.total_cents = value * 100 + self.total_cents % 100
+        else:
+            print("Error dollars")
+
+    @property
+    def cents(self):
+        return self.total_cents % 100
+
+    @cents.setter
+    def cents(self, value):
+        if 0 <= value < 100:
+            self.total_cents = self.total_cents // 100 * 100 + value
+        else:
+            print("Error cents")
+
+    def __str__(self):
+        return f"Ваше состояние составляет {self.dollars} долларов {self.cents} центов"
+    
+Bill = Money(101, 99)
+print(Bill)  # Ваше состояние составляет 101 долларов 99 центов
+print(Bill.dollars, Bill.cents)  # 101 99
+print(Bill.total_cents) # 10199
+Bill.dollars = 666
+print(Bill)  # Ваше состояние составляет 666 долларов 99 центов
+Bill.cents = 12
+print(Bill)  # Ваше состояние составляет 666 долларов 12 центов
